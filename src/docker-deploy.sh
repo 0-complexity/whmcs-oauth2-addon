bac3104c9d2864b5e5613b42aaf041b10acc13ac
Lucas Vanhalst <lucas@mobicage.com>
2016-06-13 18:20:56 +0200

    fix deploy script for production

--- src/docker-deploy.sh
+++ src/docker-deploy.sh
@@ -7,9 +7,10 @@ rm -rf /tmp/whmcs
 tar -xvf custom_oauth2.tgz
 if [ ${debug} = 'debug' ]; then
     echo "Deploying to dev server"
+    docker cp whmcs ${container_name}:/var/www/html/
 else
     mv whmcs html
     mv html/templates/itsyouonline html/templates/fusion
+    docker cp html ${container_name}:/var/www/html/
 fi
-docker cp whmcs ${container_name}:/var/www/html/
 rm -rf custom_oauth2 custom_oauth2.tgz docker-deploy.sh
